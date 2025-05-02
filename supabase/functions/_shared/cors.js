6e18acfbf5e4f0c6d3ae988a7a28795250e901b6
Manuel Peyon <57277595+PeyonManuel@users.noreply.github.com>
2025-05-02 11:48:38 +0200

    Delete supabase directory

--- supabase/functions/_shared/cors.js
+++
@@ -1,5 +0,0 @@
-export const corsHeaders = {
-  'Access-Control-Allow-Origin': '*', // Or specific origin for production
-  'Access-Control-Allow-Headers':
-    'authorization, x-client-info, apikey, content-type',
-};
